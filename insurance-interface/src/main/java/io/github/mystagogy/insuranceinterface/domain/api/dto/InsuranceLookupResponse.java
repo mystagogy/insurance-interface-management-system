71ece8ca5a5468f39d3ba0d358de16dc5f89d5a2
mystagogy <hi7747@naver.com>
2026-04-24 23:00:16 +0900

    불필요한 파일 제거

--- insurance-interface/src/main/java/io/github/mystagogy/insuranceinterface/domain/api/dto/InsuranceLookupResponse.java
+++
@@ -1,5 +0,0 @@
-package io.github.mystagogy.insuranceinterface.domain.api.dto;
-
-public record InsuranceLookupResponse(String policyNumber, String status) {
-}
-
--- insurance-interface/src/main/java/io/github/mystagogy/insuranceinterface/domain/retry/dto/RetryResultResponse.java
+++
@@ -1,5 +0,0 @@
-package io.github.mystagogy.insuranceinterface.domain.retry.dto;
-
-public record RetryResultResponse(String requestId, boolean accepted) {
-}
-
